7e32d6d088d663fa56c5cc77dca9bf414b853c40
boss-kali <gamingroodetc@gmail.com>
2026-02-27 13:23:44 +0300

    add

--- study grop/method/src/CashierAssistant.java
+++ study grop/method/src/CashierAssistant.java
@@ -4,8 +4,13 @@ public class CashierAssistant {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        System.out.print("enter your name : ");
-        String name = input.nextLine();
-        System.out.println("Hello " + name);
+        System.out.print("enter you age : ");
+        int age = input.nextInt();
+        if (age >= 18) {
+            System.out.println("You can voit ");
+        } else {
+            System.out.println("You are not old enough !");
+        }
+
     }
 }
