07518085901893881ff5283f8d999237f1596e06
boss-kali <gamingroodetc@gmail.com>
2026-02-27 13:02:09 +0300

    add

--- study grop/method/src/CashierAssistant.java
+++ study grop/method/src/CashierAssistant.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class CashierAssistant {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("enter your name : ");
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
     }
 }
