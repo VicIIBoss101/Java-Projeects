6c05efef6a8a78cc84e1e79093722798285b6f7f
boss-kali <gamingroodetc@gmail.com>
2026-03-01 20:12:37 +0300

    edit

--- study grop/Method Day/src/SimpleCalculater.java
+++ study grop/Method Day/src/SimpleCalculater.java
@@ -3,69 +3,42 @@ import java.util.Scanner;
 public class SimpleCalculater {
     static Scanner input = new Scanner(System.in);
 
-    static void additon(int number1, int number2) {
-        System.out.println();
-        int result = number1 + number2;
-        System.out.printf("%d + %d = %d", number1, number2, result);
-        System.out.println();
-    }
-
-    static void subtraction(int number1, int number2) {
-        int result = number1 - number2;
-        System.out.println();
-        System.out.printf("%d - %d = %d", number1, number2, result);
-        System.out.println();
-    }
-
-    static void multiplication(int number1, int number2) {
-        int result = number1 * number2;
-        System.out.println();
-        System.out.printf("%d × %d = %d", number1, number2, result);
-        System.out.println();
-    }
-
-    static void division(int number1, int number2) {
-        double result = (double) number1 / (double) number2;
-        if (number2 == 0) {
-            System.out.println("The second number must be not equal to zero!!");
-        } else {
-            System.out.println();
-            System.out.printf("%d ÷ %d = %d", number1, number2, result);
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
         while (true) {
+            // هنا خليت الشرط تروو حتى يبقى شغال الا يختار ايقاف يلا تزقف
             System.out.println("---- Menu ----");
             System.err.println("1. do calculation" + "\n" + "2. exit");
             System.out.print("\n" + "enter your choice: ");
             int choice = input.nextInt();
 
             switch (choice) {
+                // هاي السويتش تخص اختيار القائمة الرئيسية
 
                 case 1:
                     System.out.print("\n" + "enter the firts number: ");
                     int number1 = input.nextInt();
-                    System.out.print("\n" + "enter the  operator: ");
-                    char op = input.next().charAt(0);
+                    System.out.println("choice the  operator ");
+                    System.out.print(
+                            "\n" + "1. (+)" + "\n" + "2. (-)" + "\n" + "3. ( * )" + "\n" + "4. /" + "\n" + " : ");
+                    int op = input.nextInt();
                     System.out.print("\n" + "enter the second number: ");
                     int number2 = input.nextInt();
 
                     switch (op) {
-                        case '+':
+                        // هاي السويتش تخص اختيار العملية الحسابية
+                        case 1:
                             additon(number1, number2);
                             break;
 
-                        case '-':
+                        case 2:
                             subtraction(number1, number2);
                             break;
 
-                        case '*':
+                        case 3:
                             multiplication(number1, number2);
                             break;
 
-                        case '/':
+                        case 4:
                             division(number1, number2);
                             break;
 
@@ -82,6 +55,37 @@ public class SimpleCalculater {
                 default:
                     System.out.println("Wrong choice!!");
             }
+        }
+    }
+    // ========================================
+    // ========================================
+    // ========================================
+
+    static void additon(int number1, int number2) {
+        int result = number1 + number2;
+        System.out.println("\n" + number1 + " + " + number2 + " = " + result + "\n");
+    }
+
+    // ================================
+    static void subtraction(int number1, int number2) {
+        int result = number1 - number2;
+        System.out.println("\n" + number1 + " - " + number2 + " = " + result + "\n");
+
+    }
+
+    // ================================
+    static void multiplication(int number1, int number2) {
+        int result = number1 * number2;
+        System.out.println("\n" + number1 + " * " + number2 + " = " + result + "\n");
+    }
+
+    // ================================
+    static void division(int number1, int number2) {
+        double result = (double) number1 / (double) number2;
+        if (number2 == 0) {
+            System.out.println("The second number must not equal to zero!!");
+        } else {
+            System.out.println("\n" + number1 + " + " + number2 + " = " + result + "\n");
         }
     }
 }
