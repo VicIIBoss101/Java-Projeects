1b4800f9746bfe5ba33df704c5a9c47d5310bb25
VicIIBoss101 <abbasroyale2@gmail.com>
2026-04-17 18:30:22 +0300

    new things

--- College/lap2/src/App.java
+++ College/lap2/src/App.java
@@ -14,39 +14,42 @@ public class App {
         int[][] a = new int[n][m];
         int[][] b = new int[r][m];
         int[][] c = new int[n + r][m];
-
+        int row = 1;
         for (int i = 0; i < a.length; i++) {
-            int count = 1;
+            int col = 1;
             for (int j = 0; j < a[i].length; j++) {
-                System.out.print(count + " Enter value for a: ");
+                System.out.print("row - col " + row + "-" + col + " Enter value for a: ");
                 a[i][j] = input.nextInt();
-                count++;
+                col++;
             }
+            row++;
         }
+        row = 1;
         for (int i = 0; i < b.length; i++) {
-            int count = 1;
+            int col = 1;
             for (int j = 0; j < b[i].length; j++) {
-                System.out.print(count + " Enter value for b: ");
+                System.out.print("row - col " + row + "-" + col + " Enter value for b: ");
                 b[i][j] = input.nextInt();
-                count++;
+                col++;
             }
+            row++;
         }
 
         System.out.println("===============================" + "\n" + "A array");
-        for (int[] w : a) {
-            for (int j : w) {
+        for (int[] i : a) {
+            for (int j : i) {
                 System.out.print(j + "\t");
             }
             System.out.println();
         }
         System.out.println("===============================" + "\n" + "b array");
-        for (int[] q : b) {
-            for (int j : q) {
+        for (int[] i : b) {
+            for (int j : i) {
                 System.out.print(j + "\t");
             }
             System.out.println();
         }
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 c[i][j] = a[i][j];
@@ -59,9 +62,9 @@ public class App {
         }
 
         System.out.println("===============================" + "\n" + "c array");
-        for (int i = 0; i < c.length; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(c[i][j] + "\t");
+        for (int[] i : c) {
+            for (int j : i) {
+                System.out.print(j + "\t");
             }
             System.out.println();
         }
