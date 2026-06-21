693d38af6de902e55b84ca8b55951cba5a1f3096
VicIIBoss101 <abbasroyale2@gmail.com>
2026-06-21 14:57:30 +0300

    ignore things

---
+++ its_like_id/src/main/java/com/test_it/Main.java
@@ -0,0 +1,8 @@
+package com.test_it;
+import java.util.Scanner;
+public class Main {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
